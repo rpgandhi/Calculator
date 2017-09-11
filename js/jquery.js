4ff5a2a7bb2ecef417805044ca0a346c56d37910
Rakhee Gandhi <rpgandhi@gmail.com>
2017-09-11 16:18:24 -0700

    To save calculator for later.

---
+++ js/jquery.js
@@ -0,0 +1,18 @@
+//jQuery(document).ready(function()){
+
+jQuery("h1").click(function() {
+  parseInt(prompt("Enter a number:"));
+  parseInt(prompt("Enter another number"));
+});
+
+
+  //var number1 = parseInt(prompt("Enter a number:"));
+  //var number2 = parseInt(prompt("Enter another number"));
+  //var whatWouldLikeToDo = prompt("What would you like to do with these two numbers?");
+  //var input3 = whatWouldLikeToDo;
+  //var add = function (number1, number2) {
+  	//return number1 + number2;
+    //};
+  //var result = add(number1, number2);
+  //alert(result);
+//}
